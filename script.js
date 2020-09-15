b90973779e1017480f9ded39bdb4666583366bbc
JOE-Z-L <46850952+JOE-Z-L@users.noreply.github.com>
2020-09-15 08:36:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -111,3 +111,72 @@ progress.addEventListener(
 progress.addEventListener("mousedown", () => (mousedown = true));
 progress.addEventListener("mouseup", () => (mousedown = false));
 skipButtons.forEach((button) => button.addEventListener("click", skipSeconds));
+
+//Variaveis
+
+let container;
+let camera;
+let renderer;
+let scene;
+let model3d;
+
+function init() {
+  container = document.querySelector(".scene");
+
+  //Criar cena
+  scene = new THREE.Scene();
+
+  const fov = 35;
+  const aspect = container.clientWidth / container.clientHeight;
+  const near = 0.1;
+  const far = 1000;
+
+  //Setup da camera
+
+  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+  camera.position.set(30, -10, 150);
+  const ambient = new THREE.AmbientLight(0x404040, 7);
+  scene.add(ambient);
+
+  const light = new THREE.DirectionalLight(0xfffffff, 2);
+  light.position.set(10, 10, 30);
+  scene.add(light);
+
+  //renderer
+  renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
+  renderer.setSize(container.clientWidth, container.clientHeight);
+  renderer.setPixelRatio(window.devicePixelRatio);
+
+  container.appendChild(renderer.domElement);
+
+  //load model
+
+  let loader = new THREE.GLTFLoader();
+  loader.load("./3d/scene.gltf", function (gltf) {
+    scene.add(gltf.scene);
+    renderer.render(scene, camera);
+    model3d = gltf.scene.children[0];
+    animate();
+  });
+}
+
+function animate() {
+  requestAnimationFrame(animate);
+  model3d.rotation.z += 0.005;
+  renderer.render(scene, camera);
+}
+
+init();
+
+function onWindowResize() {
+  camera.aspect = container.clientWidth / container.clientHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(container.clientWidth, container.clientHeight);
+}
+window.addEventListener("resize", onWindowResize);
+
+//controls
+
+controls = new THREE.OrbitControls(camera, renderer.domElement);
